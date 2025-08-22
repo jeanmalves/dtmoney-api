a1a70a9652081b6a3616a713fc47e8bd54f0ed41
Jeam Alves <jeanmalves@gmail.com>
2025-08-22 08:37:31 -0400

    config zod

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  const error = z.treeifyError(_env.error)
+  console.error('⚠️ Invalid environment variables', error.properties)
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
