8c6c2644f1a6092c34caaa192c90e0fa6b197cfc
Jeam Alves <jeanmalves@gmail.com>
2025-08-23 09:35:22 -0400

    add config server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
 
-const app = fastify();
+const app = fastify()
 
-app.get('/hello', () => {
-  return 'hello world'
-});
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
+})
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('Server is running on http://localhost:3333');
-});
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server is running on http://localhost:3333')
+  })
