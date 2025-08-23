9a719dd8345215efa58bcd9bc2b9a8bb5ca0a5ab
Jeam Alves <jeanmalves@gmail.com>
2025-08-23 10:09:31 -0400

    create transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,26 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import z from 'zod'
+import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*')
-    return tables
+  app.post('/', async (reuest, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      type: z.enum(['credit', 'debit']),
+      amount: z.number(),
+    })
+
+    const { title, type, amount } = createTransactionBodySchema.parse(
+      reuest.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
