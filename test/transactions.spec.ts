db7c99320f7fdc7d880056909e13a4c38b5cc108
Jeam Alves <jeanmalves@gmail.com>
2025-09-02 08:21:43 -0400

    add test to transactions list

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,22 +1,49 @@
-import { afterAll, beforeAll, test } from 'vitest'
+import { afterAll, beforeAll, expect, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
+import { describe } from 'node:test'
 
 
-beforeAll(async () => {
-  await app.ready()
+
+describe('transactions', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit'
+      }).expect(201)
+  });
+
+  it('should be able to list transactions', async () => {
+    const responseNewTransaction = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit'
+      })
+
+    const cookies = responseNewTransaction.get('Set-Cookie') || []
+
+    const responseTransactions = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(responseTransactions.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000
+      })
+    ]);
+  });
 })
-
-afterAll(async () => {
-  await app.close()
-})
-
-test('user can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit'
-    }).expect(201)
-});
